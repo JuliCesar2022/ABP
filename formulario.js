e9063e60f80904b5cfaf9c4d7ed1de5928b90d67
JuliCesar2022 <108636544+JuliCesar2022@users.noreply.github.com>
2022-11-12 21:25:30 -0500

    fix: gran cambio

---
+++ formulario.js
@@ -0,0 +1,328 @@
+var limitar=0;
+
+jsonData = {
+
+    preguntas:[
+        
+    ],
+
+    tablaScore: [
+         
+    ]
+
+}
+
+CurrentCuestion = {
+    question: "",
+    responses: [],
+    correct: null,
+    vale: null
+}
+contar=0
+function agg(){
+
+CurrentCuestion.correct=null;
+
+    h= document.getElementById("respuestass").value
+   vallidar= h.replaceAll(" ","")
+
+    if(vallidar==""){
+        console.log("ingrese la respuesta")
+    
+        }else{
+            limitar+=1
+            if(limitar<5){
+                CurrentCuestion.responses.push(h)
+                
+                
+                reloadPage()
+
+                
+               
+                  
+                
+                
+                
+                
+                
+           
+        }
+    } 
+    
+    
+
+    document.getElementById("respuestass").value=""
+
+}
+
+
+function reloadPage(){
+   
+    
+    
+  
+    document.getElementById("lang").innerHTML=`<option value="escoja">-Escoja la respuesta correcta-</option>`
+    for( i = contar; i< CurrentCuestion.responses.length;i++ ){
+        
+        document.getElementById("resp"+i).innerHTML=`<p id="respuestaaparecer${i}">${CurrentCuestion.responses[i]}</p>`
+        document.getElementById("resp"+i).style=`margin: 10px;padding:0; background: rgba(0, 0, 0, 0.1);display: flex;
+        justify-content: center;
+        align-items: center;border-radius:20px;height:45px;opacity: 0;
+        animation: fade-in 1s forwards;"`
+          document.getElementById("respuestaaparecer"+i).style=`opacity: 0;
+        animation: fade-in 3s forwards;`
+
+
+       
+        
+        
+    }
+    contar++
+   
+    for( i = 0; i< CurrentCuestion.responses.length;i++ ){
+        document.getElementById("lang").innerHTML+=` <option value="${i}" >${CurrentCuestion.responses[i]}</option>`
+    }
+}
+
+
+
+function myFunction(){
+    CurrentCuestion.correct=null
+
+    var r= document.getElementById("lang").value
+if(r!="escoja"){
+    CurrentCuestion.correct=r
+}else{
+
+    console.log("seleccione una opcion porfavor")
+    CurrentCuestion.correct=null
+}
+
+  
+
+   console.log(CurrentCuestion.correct)
+   
+   
+}
+
+
+function actualizar(){
+
+    CurrentCuestion.question= ""
+    m=document.getElementById("questions").value
+    vallidar= m.replaceAll(" ","")
+
+    if(vallidar==""){
+        console.log("ingrese una pregunta valida")
+    
+        }else{
+            CurrentCuestion.question= ""
+            m=document.getElementById("questions").value
+            CurrentCuestion.question = m
+            console.log(CurrentCuestion.question)
+           
+        }
+    } 
+    
+function score(){
+
+    CurrentCuestion.vale= null
+    m=document.getElementById("scoree").value
+    vallidar= m.replaceAll(" ","")
+
+    if(vallidar==""){
+
+        console.log("ingrese un score valido")
+    
+        }else{
+            CurrentCuestion.vale= ""
+            m=document.getElementById("scoree").value
+            CurrentCuestion.vale= m
+            console.log(CurrentCuestion.vale)
+           
+        }
+    } 
+    
+
+    contemos=0
+    contemos2=0
+    function BotonAgregar(){
+
+
+       
+        
+        if(CurrentCuestion.question.length>0){
+            if(CurrentCuestion.responses.length>1){
+               if (CurrentCuestion.vale>0 && CurrentCuestion.vale.length>0 ){
+                if(CurrentCuestion.correct==0 || CurrentCuestion.correct==1 || CurrentCuestion.correct==2 || CurrentCuestion.correct==3 ){
+                
+                    if(contemos2==3)contemos2=0
+
+                    jsonData.preguntas.push({...CurrentCuestion})   
+
+                    console.log(jsonData.preguntas)
+                    document.getElementById("Preguntasagregadas"+contemos).innerHTML+=`
+            
+                    <div class= "visible" id="visible${contemos}">
+                    
+                    <div class="secion" >
+                
+                            <p>${ CurrentCuestion.question}<br/>${CurrentCuestion.vale}</p>
+                
+                        
+                        </div>
+            
+                    <div class="icono">
+                    <ion-icon id="icono${contemos}" name="chevron-down-outline" onclick="flechita(`+contemos+`)"></ion-icon>
+                    </div>
+                    </div>
+                    </div>
+                    <visibilidad id="visibilidad" class="visibilidad`+contemos+`"></visibilidad>
+                    `
+                    document.getElementById("Preguntasagregadas"+contemos).style=`opacity: 0; animation: fade-in-Item 1s forwards;`
+            
+                    for( i = 0; i< CurrentCuestion.responses.length;i++ ){
+                                document.getElementsByClassName("visibilidad"+contemos)[0].innerHTML+=`<p class="mejoras">${CurrentCuestion.responses[i]} </p>`
+                    
+                    
+                    
+                }
+                contemos++
+
+            }
+        } 
+     }
+
+     console.log(jsonData)
+
+
+    }
+
+       
+}
+
+    
+    function flechita(id){
+       
+            console.log(id)
+
+                resp = document.getElementsByClassName("visibilidad"+id)[0].className
+        
+                console.log(resp)
+                if(resp.includes("active")){
+                  document.getElementById("icono"+id).style="transform: rotate(0deg); transition: .7s;"
+                    console.log("si")
+                    // document.getElementsByClassName("visibilidad"+id)[0].parentElement.firstChild.style = " border-radius: 20px;"
+                    document.getElementsByClassName("visibilidad"+id)[0].className = "visibilidad"+id
+                    document.getElementsByClassName("visibilidad"+id)[0].className= "visibilidad"+id +" des"
+                    document.getElementById("visible"+id ).style = " border-radius:20px;" 
+
+
+                   
+        
+                }else{
+                    
+                    document.getElementById("visible"+id ).style = " border-radius:20px 20px 0 0;"
+                    // document.getElementsByClassName("visibilidad"+id)[0].parentElement.firstChild.firstChild.style = " border-radius:20px 20px 0px 0px;"
+
+                    document.getElementById("icono"+id).style="transform: rotate(180deg);  transition: .7s;"
+                    
+                    document.getElementsByClassName("visibilidad"+id)[0].className = "visibilidad"+id + " active"
+                    
+                    console.log("no")
+                }
+            }
+
+
+            function crearsala() {
+if(jsonData.preguntas.length>0){
+
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+      
+       fetch("https://servidor-abp-mate.herokuapp.com/CrearSala?preguntas="+JSON.stringify(jsonData), requestOptions)
+        .then(response => response.text())
+        .then(result => {
+               decode= JSON.parse(result)
+               linkidsala= decode.data.idSala
+                
+               Swal.fire({
+                html:` <div class="link">
+                <div class="link1"> <a id="copiartexto" href="http://127.0.0.1:5501/juego/?id=${linkidsala}"> http://127.0.0.1:5501/juego/?id=${linkidsala}</a> </div>
+                <div class="copy" onclick="copiar()"><ion-icon name="copy-outline"></ion-icon></div>
+            </div>`,
+            style:`margin:0;padding:0;`,
+            
+
+                showClass: {
+                  popup: 'animate__animated animate__fadeInDown'
+                },
+                hideClass: {
+                  popup: 'animate__animated animate__fadeOutUp'
+                }
+              })
+         
+
+        })
+        .catch(error => console.log('error', error));
+
+    
+      
+    console.log(jsonData)
+ }
+                
+            }
+           
+
+
+
+
+            function Juego(){
+                var requestOptions = {
+                    method: 'GET',
+                    redirect: 'follow'
+                  };
+                  
+
+              var idsala= document.getElementById("idsala").value
+                if(idsala.length>0){
+                  
+                    fetch("https://servidor-abp-mate.herokuapp.com/GetSalaById?id="+idsala, requestOptions)
+                    .then(response => response.text())
+                    .then(result => {
+                        decode= JSON.parse(result)
+                        linkidsala= decode.data
+                        // preguntasdata=decode.data.preguntas
+                        
+
+                        if(linkidsala!=null ){
+                            location.href ="http://127.0.0.1:5501/juego/?id="+idsala;
+                        }else {
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'Oops...',
+                                text: 'Something went wrong!',
+                                footer: '<a href="">Why do I have this issue?</a>'
+                              })
+                        }
+                    })
+                    .catch(error => console.log('error', error));
+                        
+                }else{
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Something went wrong!',
+                        footer: '<a href="">Why do I have this issue?</a>'
+                      })
+                }
+               
+            }
+
+            function copiar(){
+                var codigoACopiar = document.getElementById('copiartexto');
+navigator.clipboard.writeText(codigoACopiar.innerHTML)
+            }
+// TODO ESTA MALO, TODO ESO ESTA MALISIMO, MALOOOOO BONIFACIO, QUE COSA MALA, BUUUUUUUUUU
---
+++ juego/index.js
@@ -0,0 +1,216 @@
+
+    formato = null
+    Validar=true
+    console.log("funciona")
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+      
+      fetch("https://servidor-abp-mate.herokuapp.com/GetSalaById?id="+getParameterByName("id"), requestOptions)
+        .then(response => response.text())
+        .then(result => { 
+            formato=JSON.parse(result)
+            formato=formato.data
+            if(formato==null){
+                location.href ="http://127.0.0.1:5501/pagina_principa/psps.html#tab2";
+            }
+        })
+
+
+
+    Swal.fire({
+        allowOutsideClick: false,
+
+        title: "Elige un Apodo",
+        
+    input: 'text',
+    
+    confirmButtonText: 'Iniciar',
+        showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+         
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        }
+      })
+      
+      .then((result) => {
+        
+        /* Read more about isConfirmed, isDenied below */
+        
+        if (result.isConfirmed) {
+           
+          if(result.value.length>3){
+            Validar=true
+            console.log("funciona")
+            var requestOptions = {
+                method: 'GET',
+                redirect: 'follow'
+              };
+              
+              fetch("https://servidor-abp-mate.herokuapp.com/GetSalaById?id="+getParameterByName("id"), requestOptions)
+                .then(response => response.text())
+                .then(result => { 
+                    formato=JSON.parse(result)
+                    formato=formato.data
+                   
+                     
+                    console.log(formato)
+            
+                    preguntas = formato["preguntas"]
+            correct= 0;
+            incorrec=0;
+            puntofinal=0;
+            time = 10
+            currentQuestion=0;
+            currentTime = time
+            
+            
+            
+              
+            
+            
+            
+            
+            
+            
+                
+                for (let index = 3; index >= -1; index--) {
+                
+                    setTimeout (
+                        ()=>{
+            
+                            if(index==0) {
+                                document.getElementById("numero").innerHTML= `GO!`;
+                            }else if(index==-1){
+                                document.getElementById("mama").remove()
+                                empezar();
+                            }else{
+                                document.getElementById("numero").innerHTML= `${index}`;
+                            }
+            
+                         
+                        },5000 - ( index * 1000) 
+                    )
+                
+                    
+                   
+                }
+            
+            
+            
+            
+            
+            
+                })
+                .catch(error => console.log('error', error));
+            
+          }else{
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Something went wrong!',
+                footer: '<a href="">Why do I have this issue?</a>'
+              }).then((siono) => {
+        
+                /* Read more about isConfirmed, isDenied below */
+                
+                if (siono.isConfirmed) {
+
+
+              location.reload()
+          }
+              })
+            }
+        } else if (result.isDenied) {
+          Swal.fire('Changes are not saved', '', 'info')
+        }
+      })
+    
+
+
+
+
+    function esperar1s(){
+        return new Promise(resolve=>{setTimeout(()=>{resolve("resolved")},1000)})
+   } 
+
+
+   function select1(a){
+    if( preguntas[currentQuestion].responses[a] == preguntas[currentQuestion].responses[ preguntas[currentQuestion].correct ] ){
+        console.log("RESPUETAS CORRECTA!!!!!!!")
+        puntosbueno = document.getElementsByClassName("correct")[0].innerHTML = (parseInt(document.getElementsByClassName("correct")[0].innerHTML) || 0 )+ parseInt( preguntas[currentQuestion].vale )
+
+        
+    }else{
+        console.log("Brutooo!")
+        puntosbueno = document.getElementsByClassName("nocorre")[0].innerHTML
+puntosbueno = parseInt(puntosbueno);
+document.getElementsByClassName("nocorre")[0].innerHTML= puntosbueno+1
+    }
+
+    currentTime = 0
+    document.getElementsByClassName("time")[0].style=`width:0%; `
+}
+
+
+
+async function empezar(){
+    console.log("empezaron los sergios a bobear")
+    console.log(preguntas.length)
+    for(i=0 ; i<=preguntas.length-1; i++){
+
+        currentQuestion = i
+        console.log(i)
+
+        div = document.createElement("h1")
+
+        div.innerHTML= preguntas[i].question
+
+     
+        //creando respuestas
+        for (a=0;a<= preguntas[i].responses.length-1;a++){
+            
+             btn= document.createElement("div")
+             btn.innerHTML= preguntas[i].responses[a]
+             btn.classList.add("btn-resp");
+
+             
+             btn.setAttribute("onclick", `select1(${a})`);
+
+
+
+             document.getElementById("resp").appendChild(btn)
+
+        }
+
+        papi = document.getElementsByClassName("hijito")[0]
+
+        papi.appendChild(div)
+
+            for(currentTime=time;currentTime>= 0;currentTime--){
+
+                document.getElementsByClassName("time")[0].style=`width:${currentTime}0%; `
+                console.log(await esperar1s())
+            }
+
+        div.remove()    
+
+        for (a=preguntas[i].responses.length-1;a>= 0;a--){
+            
+           document.getElementsByClassName("btn-resp")[0].remove()
+
+       }
+    }
+
+}
+
+    function getParameterByName(name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+
